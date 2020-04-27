dd45dbbdbd83b2be50dc86d6e49a71765d6766e2
Jacob Granley <jgranley@mymail.mines.edu>
2020-04-27 09:10:23 -0600

    additive works now... but gets absolutely terrible r^2 values

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ from keras.callbacks import EarlyStopping
 def load_data(input_folder, one_hot_encode=False, do_name=True):
     cols = ['match_name', 'marathon', 'time_minutes', 'vaporfly'] # not using year column 
 
-    dtypes = {'vaporfly' : np.float64, 'time_minutes' : np.float64} # convert true/false to 1/0 and make sure time is a float
+    dtypes = {'time_minutes' : np.float64} # convert true/false to 1/0 and make sure time is a float
 
     files = os.listdir(input_folder)
     csvs = [f for f in files if f[-4:] == '.csv']
@@ -137,19 +137,24 @@ def get_vaporfly_inputs(x, y, inverse=False):
     return x_ret, y_ret
 
 
-def split_gender(x, y, cols):
-    indices_f = [i for i in range(len(x[0])) if x[0][i][cols.index('gender')] == 0]
-    indices_m = [i for i in range(len(x[0])) if x[0][i][cols.index('gender')] == 1]
+def split_gender(x, cols, y=None):
+    indices_f = [i for i in range(len(x[0])) if x[0][i][cols.index('gender') -1] == 1]
+    indices_m = [i for i in range(len(x[0])) if x[0][i][cols.index('gender') -1] == 0]
     # print(indices_f)
     # print(indices_m)
 
     x_m = [x[0][indices_m], x[1][indices_m], x[2][indices_m]]
-    y_m = y[indices_m]
+    if y is not None:
+        y_m = y[indices_m]
 
     x_f = [x[0][indices_f], x[1][indices_f], x[2][indices_f]]
-    y_f = y[indices_f]
+    if y is not None:
+        y_f = y[indices_f]
 
-    return x_m, x_f, y_m, y_f
+    if y is not None:
+        return x_m, x_f, y_m, y_f
+    else:
+        return x_m, x_f
 
 def get_feedforward_model(x):
     # right now, use network with 4 hidden layers with 50 nodes each
@@ -223,11 +228,11 @@ def r2_keras(y_true, y_pred):
 def compare_models():
     input_folder = 'data'
 
-    x, y, cols = get_data(input_folder, one_hot_encode=True, do_name=True, return_cols=True)
+    x, y, cols = get_data(input_folder, one_hot_encode=True, do_name=False, return_cols=True)
     # print (cols)
 
-    # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.15, shuffle=True)
-    x_train, x_test, y_train, y_test = get_train_test(x, y, cols)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.15, shuffle=True)
+    # x_train, x_test, y_train, y_test = get_train_test(x, y, cols)
 
     linear_model = LinearRegression()
     linear_model.fit(x_train, y_train)
@@ -244,44 +249,70 @@ def compare_models():
     nn_test_r2 = nn.evaluate(x_test, y_test)[1]
 
 
-    # now do additive network
-    shoe_effects = ['gender']
-    an, activation_network = get_additive_model(cols, shoe_effects=shoe_effects)
+    # now do additive network, one for each male and female
+    shoe_effects = ['gender', 'race_']
+    an_m, activation_network_m = get_additive_model(cols, shoe_effects=shoe_effects)
+    an_f, activation_network_f = get_additive_model(cols, shoe_effects=shoe_effects)
+
+
     # construct input vectors
-    x_train_an = get_additive_inputs(x_train, cols, shoe_effects=shoe_effects)
-    x_test_an = get_additive_inputs(x_test, cols, shoe_effects=shoe_effects)
+    male_indices = [i for i in range(len(x)) if x[i][cols.index('gender')] == 0]
+    female_indices = [i for i in range(len(x)) if x[i][cols.index('gender')] == 1]
 
-    an.compile(optimizer='adam', loss='mean_squared_error', metrics=[r2_keras])
-    es = EarlyStopping(monitor='val_r2_keras', patience=20, mode='max')
+    x_m = x[male_indices]
+    y_m = y[male_indices]
+    x_f = x[female_indices]
+    y_f = y[female_indices]
 
-    hist = an.fit(x=x_train_an, y=y_train, batch_size = 32, epochs=500, validation_data = (x_test_an, y_test), callbacks=[], verbose=0)
-    an_train_r2 = an.evaluate(x_train_an, y_train)[1]
-    an_test_r2 = an.evaluate(x_test_an, y_test)[1]
+    x_train_m, x_test_m, y_train_m, y_test_m = train_test_split(x_m, y_m, test_size=0.05, shuffle=True)
+    x_train_f, x_test_f, y_train_f, y_test_f = train_test_split(x_f, y_f, test_size=0.05, shuffle=True)
+    # x_train_m, x_test_m, y_train_m, y_test_m = get_train_test(x_m, y_m, cols, rand=0.05)
+    # x_train_f, x_test_f, y_train_f, y_test_f = get_train_test(x_f, y_f, cols, rand=0.05)
+
+    x_train_m = get_additive_inputs(x_train_m, cols, shoe_effects=shoe_effects)
+    x_test_m = get_additive_inputs(x_test_m, cols, shoe_effects=shoe_effects)
+    x_train_f = get_additive_inputs(x_train_f, cols, shoe_effects=shoe_effects)
+    x_test_f = get_additive_inputs(x_test_f, cols, shoe_effects=shoe_effects)
+
+    # train male
+    an_m.compile(optimizer='adam', loss='mean_squared_error', metrics=[r2_keras])
+    es = EarlyStopping(monitor='val_r2_keras', patience=15, mode='max')
+
+    hist = an_m.fit(x=x_train_m, y=y_train_m, batch_size = 32, epochs=500, validation_data = (x_test_m, y_test_m), callbacks=[], verbose=0)
+    an_train_r2_m = an_m.evaluate(x_train_m, y_train_m)[1]
+    an_test_r2_m = an_m.evaluate(x_test_m, y_test_m)[1]
+
+    # train female
+    an_f.compile(optimizer='adam', loss='mean_squared_error', metrics=[r2_keras])
+    es = EarlyStopping(monitor='val_r2_keras', patience=15, mode='max')
+
+    hist = an_f.fit(x=x_train_f, y=y_train_f, batch_size = 32, epochs=500, validation_data = (x_test_f, y_test_f), callbacks=[], verbose=0)
+    an_train_r2_f = an_f.evaluate(x_train_f, y_train_f)[1]
+    an_test_r2_f = an_f.evaluate(x_test_f, y_test_f)[1]
+
 
     # get activations for vaporfly and non vaporfly
     # not really sure if i should be doing this on train data, test data, or both
-    vap_x, vap_y = get_vaporfly_inputs(x_train_an, y_train)
-    nonvap_x, nonvap_y = get_vaporfly_inputs(x_train_an, y_train, inverse=True)
+    vap_x_m, vap_y_m = get_vaporfly_inputs(x_train_m, y_train_m)
+    nonvap_x_m, nonvap_y_m = get_vaporfly_inputs(x_train_m, y_train_m, inverse=True)
 
-    # need to also divide by gender, or else there isnt any visible vaporfly effect
-    vap_x_m, vap_x_f, vap_y_m, vap_y_f = split_gender(vap_x, vap_y, cols)
-    nonvap_x_m, nonvap_x_f, nonvap_y_m, nonvap_y_f = split_gender(nonvap_x, nonvap_y, cols)
+    vap_x_f, vap_y_f = get_vaporfly_inputs(x_train_f, y_train_f)
+    print(len(vap_y_f))
+    nonvap_x_f, nonvap_y_f = get_vaporfly_inputs(x_train_f, y_train_f, inverse=True)
 
 
-    preds_vap_m = activation_network.predict(vap_x_m)
-    preds_nonvap_m = activation_network.predict(nonvap_x_m)
-    preds_total_vap_m = an.predict(vap_x_m)
-    preds_total_nonvap_m = an.predict(nonvap_x_m)
+    preds_vap_m = activation_network_m.predict(vap_x_m)
+    preds_nonvap_m = activation_network_m.predict(nonvap_x_m)
+    preds_total_vap_m = an_m.predict(vap_x_m)
+    preds_total_nonvap_m = an_m.predict(nonvap_x_m)
 
-    preds_vap_f = activation_network.predict(vap_x_f)
-    preds_nonvap_f = activation_network.predict(nonvap_x_f)
-    preds_total_vap_f = an.predict(vap_x_f)
-    preds_total_nonvap_f = an.predict(nonvap_x_f)   
+    preds_vap_f = activation_network_f.predict(vap_x_f)
+    preds_nonvap_f = activation_network_f.predict(nonvap_x_f)
+    preds_total_vap_f = an_f.predict(vap_x_f)
+    preds_total_nonvap_f = an_f.predict(nonvap_x_f)   
 
-    female_vap = [y[i] for i in range(len(vap_y)) if vap_x[0][i][cols.index('gender')] == 0]
-    male_vap = [y[i] for i in range(len(vap_y)) if vap_x[0][i][cols.index('gender')] == 1]
-    female_nonvap = [y[i] for i in range(len(nonvap_y)) if nonvap_x[0][i][cols.index('gender')] == 0]
-    male_nonvap = [y[i] for i in range(len(nonvap_y)) if nonvap_x[0][i][cols.index('gender')] == 1]
+    y_train_vap = np.concatenate([vap_y_m, vap_y_f])
+    y_train_nonvap = np.concatenate([nonvap_y_m, nonvap_y_f])
 
     # print(cols)
     print("Linear Model:")
@@ -295,10 +326,11 @@ def compare_models():
 
     print("Additive Network Model:")
     print("\tShoe Effects: " + str(shoe_effects))
-    print("\tTrain R Squared: " + str(an_train_r2))
-    print("\tTest R Squared: " + str(an_test_r2))
+    print("\tR Squared Male: train=" + str(an_train_r2_m) + ', test=' + str(an_test_r2_m))
+    print("\tR Squared Female: train=" + str(an_train_r2_f) + ', test=' + str(an_test_r2_f))
 
     print("Male")
+    print("\tCounts: Train=" + str(len(y_train_m)) + ", Test=" + str(len(y_test_m)))
     print("\tActivations Vaporfly:")
     print("\t\tNormal: mean=" + str(np.mean(preds_vap_m[0])) + ", std=" + str(np.std(preds_vap_m[0])))
     print("\t\tShoe: mean=" + str(np.mean(preds_vap_m[1])) + ", std=" + str(np.std(preds_vap_m[1])))
@@ -309,6 +341,7 @@ def compare_models():
     print("\t\tTotal: mean=" + str(np.mean(preds_total_nonvap_m)) + ", std=" + str(np.std(preds_total_nonvap_m)))
 
     print("Female")
+    print("\tCounts: Train=" + str(len(y_train_f)) + ", Test=" + str(len(y_test_f)))
     print("\tActivations Vaporfly:")
     print("\t\tNormal: mean=" + str(np.mean(preds_vap_f[0])) + ", std=" + str(np.std(preds_vap_f[0])))
     print("\t\tShoe: mean=" + str(np.mean(preds_vap_f[1])) + ", std=" + str(np.std(preds_vap_f[1])))
@@ -320,14 +353,14 @@ def compare_models():
 
 
     print("Actual times vaporfly:")
-    print("\tTotal: mean=" + str(np.mean(vap_y)) + ", std=" + str(np.std(vap_y)))
-    print("\tMale: mean=" + str(np.mean(male_vap)) + ", std=" + str(np.std(male_vap)))
-    print("\tFemale: mean=" + str(np.mean(female_vap)) + ", std=" + str(np.std(female_vap)))
+    print("\tTotal: mean=" + str(np.mean(y_train_vap)) + ", std=" + str(np.std(y_train_vap)))
+    print("\tMale: mean=" + str(np.mean(vap_y_m)) + ", std=" + str(np.std(vap_y_m)))
+    print("\tFemale: mean=" + str(np.mean(vap_y_f)) + ", std=" + str(np.std(vap_y_f)))
 
     print("Actual times nonvaporfly:")
-    print("\tTotal: mean=" + str(np.mean(nonvap_y)) + ", std=" + str(np.std(nonvap_y)))
-    print("\tMale: mean=" + str(np.mean(male_nonvap)) + ", std=" + str(np.std(male_nonvap)))
-    print("\tFemale: mean=" + str(np.mean(female_nonvap)) + ", std=" + str(np.std(female_nonvap)))
+    print("\tTotal: mean=" + str(np.mean(y_train_nonvap)) + ", std=" + str(np.std(y_train_nonvap)))
+    print("\tMale: mean=" + str(np.mean(nonvap_y_m)) + ", std=" + str(np.std(nonvap_y_m)))
+    print("\tFemale: mean=" + str(np.mean(nonvap_y_f)) + ", std=" + str(np.std(nonvap_y_f)))
     
 
 
